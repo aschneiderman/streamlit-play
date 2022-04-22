4d3ca053dd78405ac3c87a2792ea4496396078c6
Anders Schneiderman <aschneiderman@users.noreply.github.com>
2022-04-22 07:46:21 -0400

    Create first code

---
+++ test.py
@@ -0,0 +1,28 @@
+print("this is a test")
+
+# %windir%\System32\cmd.exe "/K" C:\Users\aschn\anaconda3\Scripts\activate.bat C:\Users\aschn\anaconda3
+
+
+# I need to figure out how to run Conda inside Visual Studio code
+# Or how to use Visual Studio code with a virtual environment other than Conda
+
+# Or, how to run streamlet from Visual Studio code on Windows 10
+
+# Or do I?
+
+# NOTTE: I created a virtual environment using Conda called sandbox
+
+
+ # Or, Uninsstall Anaconda, then follow the instructions to uuse Miniconda 
+#  https://docs.conda.io/en/latest/miniconda.html
+
+#  https://share.streamlit.io/streamlit/30days
+
+
+
+# Best approach:
+# https://www.youtube.com/watch?v=Yk-unX4KnV4
+
+# Basically, he starts from anaconda prompt, activates it, and then gets going
+# Because streamlet doesn't require you to keep ruI'm going to need
+# I'm going to need a different workflow that I normally use 
