dc0a8603267ab10299cc5f40ce0167b5a7c598b6
Anders Schneiderman <aschneiderman@users.noreply.github.com>
2022-07-25 14:59:33 -0400

    Revise, get going for Torvic

--- app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.write("Hey kids!")
-
-st.write("How ya doin")
---
+++ test/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.write("Hey kids!")
+
+st.write("How ya doin")
+
+# Now create a batch file that I can run from the desktop
